4fb6c308249cebaae4fbdd0c73298f8925faaea7
vaibhav100995 <vaibhav100995@gmail.com>
2023-12-31 13:52:25 +0530

    conditional code added : "https://www.youtube.com/watch?v=20xIeaJ33gs&list=PLS1QulWo1RIZ-IvP8n2ArWXj4s6ch50T-&index=2"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,38 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class DemoApplication extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User oAuth2User){
+		return Collections.singletonMap("name", oAuth2User.getAttribute("login"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception{
+		httpSecurity.authorizeHttpRequests(a -> a.
+				antMatchers("/","/error","/webjars/**").permitAll().
+				anyRequest().authenticated()).
+				exceptionHandling(e -> e.
+						authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))).
+				oauth2Login();
+	}
 }
